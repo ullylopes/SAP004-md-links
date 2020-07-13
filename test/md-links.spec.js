9885f1f798df6384069fa5c46bdd61b76d4a0695
Laboratoria <you@example.com>
2020-07-13 12:00:30 -0300

    tentativa de teste

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,8 @@
-const mdLinks = require('../lib');
+const mdLinks = require('../index.js');
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe(mdLinks, () => {
+  it('should be a function', () => {
+    expect(typeof mdLinks).toBe('function');
   });
-
-});
+  it('returns array', (done) => {
+    mdLinks
